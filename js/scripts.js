e00346dc585be8b3793be2060293bc5c8bc4046c
Paige Williams and Robert Bruce <robert@>
2018-10-23 14:35:07 -0700

    Add checkboxes and display results based on how boxes checked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,64 @@
 //back-end
-// convert string to array
-function createArray(unprocessedString){
-  var unprocessedArray = unprocessedString.split("");
-  return unprocessedArray;
+
+function countArray(inputArray) {
+  return inputArray.length;
+
 }
 
-// replace vowels with dashes with splice
-function replaceVowels(string){
-  var array = createArray(string)
-  for (var i = 0; i < array.length; i ++){
-    if ((array[i] === "a") || (array[i] === "e") ||(array[i] === "i") || (array[i] === "o") || (array[i] === "u")){
-      array.splice(array.indexOf(array[i]), 1, "-")
-    }
-    // console.log(array);
+
+var highStressTitle = "High Stress Coping Methods"
+var highStressBody = "High Stress Coping Methods"
+var lowStressTitle = "Low Stress Coping Methods"
+var lowStressBody = "Low Stress Coping Methods"
+
+function generateResults(inputArray) {
+  if (countArray(inputArray) >= 4) {
+    displayResults(highStressTitle, highStressBody);
+    //display high stress results
+
+  } else if (countArray(inputArray) >= 1) {
+    displayResults(lowStressTitle, lowStressBody);
+    //display low stress options
+  } else {
+    //you are healthy
   }
-
-  return array.join("");
 }
 
+// front-end
 
-// without splice
-function createNewArray(array){
-  var processedArray = []
+function displayResults(arg1, arg2) {
+  $("#results").slideDown();
+  $("#methods div h4").text(arg1);
+  $("#panel-body").text(arg2);
+};
 
-  for (var i = 0; i < array.length; i ++){
-    //replace vowels
-    if ((array[i] === "a") || (array[i] === "e") ||(array[i] === "i") || (array[i] === "o") || (array[i] === "u")){
-      processedArray.push("-");
-    } else {
-      processedArray.push(array[i]);
-    }
-  }
-  return(processedArray);
-}
+
+// function displayHighStressResults() {
+//   $("#results").slideDown();
+//   $("#methods div h4").text("High Stress Coping Methods");
+//   $("#panel-body").text("You should meditate and pet a cat");
+// };
+//
+// function displayLowStressResults() {
+//   $("#results").slideDown();
+//   $("#methods div h4").text("Low Stress Coping Methods");
+//   $("#panel-body").text("You should meditate.");
+// };
+
 
 $(function() {
+  $("#input").submit(function(event) {
+    event.preventDefault();
+    var boxesChecked = [];
+    $("input:checkbox[name=warning-signs]:checked").each(function() {
+      boxesChecked.push($(this).val());
+    })
+    $("input:checkbox[name=health-symptoms]:checked").each(function() {
+      boxesChecked.push($(this).val());
+    })
+    generateResults(boxesChecked);
+
+  });
 
 
 
